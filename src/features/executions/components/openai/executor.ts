5b228b40a20d7930dcd8a71e5d0fbe79ac86a67c
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2025-11-19 16:53:37 +0100

    encrypting credentials

--- src/features/executions/components/openai/executor.ts
+++ src/features/executions/components/openai/executor.ts
@@ -5,6 +5,7 @@ import { createOpenAI } from "@ai-sdk/openai"
 import { generateText } from "ai"
 import { openAiChannel } from "@/inngest/channels/openai";
 import prisma from "@/lib/db";
+import { decrypt } from "@/lib/encryption";
 
 Handlebars.registerHelper("json", (context) => {
     const jsonString = JSON.stringify(context, null, 2);
@@ -80,7 +81,7 @@ export const openAiExecutor: NodeExecutor<OpenAiData> = async ({
         })
     });
 
-    if (!credential) {  
+    if (!credential) {
         await publish(
             openAiChannel().status({
                 nodeId,
@@ -91,7 +92,7 @@ export const openAiExecutor: NodeExecutor<OpenAiData> = async ({
     }
 
     const openai = createOpenAI({
-        apiKey: credential.value
+        apiKey: decrypt(credential.value)
     });
 
     try {
